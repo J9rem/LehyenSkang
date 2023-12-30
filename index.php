e19f176ce6873eee76cc542830e7db6e8121cb59
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-12-30 22:46:07 +0100

    fix(composer): import right theme

--- index.php
+++ index.php
@@ -5,13 +5,21 @@
  * Author: Jérémy Dufraisse 2023
  */
 
+use LehyenSkang\Pico;
+
 if (is_file(__DIR__ . '/vendor/autoload.php')) {
     include_once(__DIR__ . '/vendor/autoload.php');
 }
 
-echo (new Pico(
-    __DIR__,
-    'vendor/picocms/pico/content-sample/',  // content dir
-    'vendor/picocms/plugins/', // plugins dir
-    'vendor/picocms/themes/'   // themes dir
-    ))->run();
+try {
+    $pp = new Pico(
+        __DIR__,
+        'vendor/picocms/pico/content-sample/',  // content dir
+        'vendor/picocms/plugins/', // plugins dir
+        'vendor/picocms/themes/'   // themes dir
+    );
+    echo ($pp)->run();
+} catch (Throwable $th) {
+    echo $th->__toString();
+}
+
